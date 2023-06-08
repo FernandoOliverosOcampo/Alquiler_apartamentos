e1dd0888141c5119ec001f0590b467f7d6d6c816
Fernando Oliveros Ocampo <53822395+FernandoOliverosOcampo@users.noreply.github.com>
2023-06-08 16:11:28 -0500

    Add files via upload

--- js/mapa.js
+++ js/mapa.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([11.2404, -74.2110], 14); // Coordenadas de Santa Marta, Colombia y nivel de zoom inicial
+var map = L.map('map').setView([11.2404, -74.2110], 18); // Coordenadas de Santa Marta, Colombia y nivel de zoom inicial
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 18,
+  maxZoom: 25,
 }).addTo(map);
 
 var marker = L.marker([11.240932, -74.199671]).addTo(map); // Coordenadas de la Catedral de Santa Marta
