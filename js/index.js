4acfc513f171315abbf7ea90dc5b8ab65ff30706
Fernando Oliveros Ocampo <53822395+FernandoOliverosOcampo@users.noreply.github.com>
2023-06-21 15:59:43 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -109,7 +109,44 @@ const Controlador = {
     });
 
 
+  },
+  slider_lugar: function(){
+    
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	// init: false,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+
+  
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	  1240: {
+		slidesPerView: 4,
+		spaceBetween: 50,
+	  },
+	} 
+    });
   }
+ 
 
 }
 
@@ -239,6 +276,7 @@ document.addEventListener('DOMContentLoaded', function () {
   Controlador.obtenerTodosAlquileres();
   Controlador.transitionSmooth();
   Controlador.btn_whatsapp();
+  Controlador.slider_lugar();
 
   var slider = tns({
     container: '#mi-slider',
@@ -251,6 +289,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   });
 
-})
+  
 
+})
 
