512cd4aac9c199c12d35a8ee170974943998c96b
Fernando Oliveros Ocampo <53822395+FernandoOliverosOcampo@users.noreply.github.com>
2023-07-25 19:10:26 -0500

    actualización modal responsive

--- js/supabase/config.js
+++ js/supabase/config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const config = {
     headers: {
       apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpeXJ3Ymp5cnBzcGNlamFreXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5ODc0MTAsImV4cCI6MjAwMTU2MzQxMH0.rzMAvDvdXMBH7PGCaenMDr7dqmjRGwQ1fbnS-12cqI0',
@@ -5,4 +6,13 @@ const config = {
     }
   };
 
+=======
+const config = {
+    headers: {
+      apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpeXJ3Ymp5cnBzcGNlamFreXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5ODc0MTAsImV4cCI6MjAwMTU2MzQxMH0.rzMAvDvdXMBH7PGCaenMDr7dqmjRGwQ1fbnS-12cqI0',
+      Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpeXJ3Ymp5cnBzcGNlamFreXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5ODc0MTAsImV4cCI6MjAwMTU2MzQxMH0.rzMAvDvdXMBH7PGCaenMDr7dqmjRGwQ1fbnS-12cqI0'
+    }
+  };
+
+>>>>>>> b621d558068f6bd40b1338fa32e9ae44615f1120
   export default config;
