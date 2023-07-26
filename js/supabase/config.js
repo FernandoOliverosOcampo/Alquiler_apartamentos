01b6f158f581dfef93efa5498848d6c3b289fc5f
Fernando Oliveros Ocampo <53822395+FernandoOliverosOcampo@users.noreply.github.com>
2023-07-25 19:29:25 -0500

    Add files via upload

--- js/supabase/config.js
+++ js/supabase/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const config = {
     headers: {
       apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpeXJ3Ymp5cnBzcGNlamFreXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5ODc0MTAsImV4cCI6MjAwMTU2MzQxMH0.rzMAvDvdXMBH7PGCaenMDr7dqmjRGwQ1fbnS-12cqI0',
@@ -6,13 +5,4 @@ const config = {
     }
   };
 
-=======
-const config = {
-    headers: {
-      apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpeXJ3Ymp5cnBzcGNlamFreXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5ODc0MTAsImV4cCI6MjAwMTU2MzQxMH0.rzMAvDvdXMBH7PGCaenMDr7dqmjRGwQ1fbnS-12cqI0',
-      Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpeXJ3Ymp5cnBzcGNlamFreXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5ODc0MTAsImV4cCI6MjAwMTU2MzQxMH0.rzMAvDvdXMBH7PGCaenMDr7dqmjRGwQ1fbnS-12cqI0'
-    }
-  };
-
->>>>>>> b621d558068f6bd40b1338fa32e9ae44615f1120
   export default config;
